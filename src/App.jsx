c9cc3850ae1cc3ef1d42696c5eabd629b85d84aa
Kashif Khan <KashifKhnx04@gmail.com>
2023-10-20 18:59:06 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <h1 className="text-heading font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'btn-clr': '#4D5B9E',
+      'dark-clr': '#293264',
+      'light-clr': '#F5F7FB',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
